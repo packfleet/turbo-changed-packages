ed581bc79fd53f65e6529a636c96fa9d406da2f5
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2024-06-05 16:38:38 +0100

    Run 'npm run all' to fix formatting issues (#42)
    * run 'npm run all'
    
    * and env var

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,34 @@
-import { execSync } from 'child_process'
-import { join } from 'path'
-import { getInput, debug, setFailed, setOutput } from '@actions/core'
+import {debug, getInput, setFailed, setOutput} from '@actions/core'
+import {execSync} from 'child_process'
+import {join} from 'path'
 
 const run = async (): Promise<void> => {
   try {
     // Get Inputs
-    const prefix = getInput('prefix', { required: false })
-    const extractPrefix = getInput('extract-prefix', { required: false })
-    const from = getInput('from', { required: false })
-    const to = getInput('to', { required: false })
-    const workingDirectory = getInput('working-directory', { required: false })
+    const prefix = getInput('prefix', {required: false})
+    const extractPrefix = getInput('extract-prefix', {required: false})
+    const from = getInput('from', {required: false})
+    const to = getInput('to', {required: false})
+    const workingDirectory = getInput('working-directory', {required: false})
 
-    debug(`Inputs: ${JSON.stringify({ prefix, from, to, workingDirectory })}`)
+    debug(`Inputs: ${JSON.stringify({prefix, from, to, workingDirectory})}`)
 
     const json = execSync(
-      `npx turbo run build ${from || to ? `--filter="[${from}...${to}]"` : ''} --dry-run=json`,
+      `npx turbo run build ${
+        from || to ? `--filter="[${from}...${to}]"` : ''
+      } --dry-run=json`,
       {
         cwd: join(process.cwd(), workingDirectory),
-        encoding: 'utf-8',
-      },
+        encoding: 'utf-8'
+      }
     )
 
     debug(`Output from Turborepo: ${json}`)
 
     const parsedOutput = JSON.parse(json)
-    const changedPackages = parsedOutput.packages.filter(
-      (p: string) => !prefix || p.startsWith(prefix)
-    ).map((p: string) => extractPrefix ? p.replace(prefix, "") : p);
+    const changedPackages = parsedOutput.packages
+      .filter((p: string) => !prefix || p.startsWith(prefix))
+      .map((p: string) => (extractPrefix ? p.replace(prefix, '') : p))
 
     setOutput('changed', changedPackages)
   } catch (error) {
